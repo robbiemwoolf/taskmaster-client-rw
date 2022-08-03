3c9886fb8d621d6809b6f357128acc6297e26199
Robbie Woolf <robbiemwoolf@gmail.com>
2022-08-03 13:14:13 -0700

    changed checkbox color to dark red

--- src/utils/tasklist/TaskLayout.js
+++ src/utils/tasklist/TaskLayout.js
@@ -1,7 +1,9 @@
+import './TaskLayout.css'
+
 export default function TaskLayout({task}) {
     return (
         <ul className='pt-3'>
-            <li>
+            <li className='container'>
                 <label className='form-check-label task' htmlFor='checktask'>
                 <input className='form-check-input checkbox mx-2' type='checkbox' id='checktask'/>
                     {task}

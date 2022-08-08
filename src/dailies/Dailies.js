ceebc545bc323921bc0523ca5f91a84f3ece5289
Robbie Woolf <robbiemwoolf@gmail.com>
2022-08-07 19:52:11 -0700

    post requests working, daily tasks displaying properly, brutal and longterm are stopping at two tasks each

--- src/dailies/Dailies.js
+++ src/dailies/Dailies.js
@@ -5,12 +5,21 @@ import Archived from "../utils/archived/Archived"
 export default function Dailies() {
     const title = 'Daily Smashed'
     const path = '/daily'
-    
+    const initialFormState = {
+        task_description: '',
+        task_daily: true,
+        task_brutal: false,
+        task_longterm: false,
+        task_archived_brutal: false,
+        task_archived_daily: false,
+        task_archived_longterm: false,
+    }
+
     return (
         <>
-        <Tasklist path={path} />
+        <Tasklist path={path} initial={initialFormState} />
         <Line />
-        <Archived path={path} title={title}/>
+        <Archived path={path} title={title} />
         </>
     )
 }
